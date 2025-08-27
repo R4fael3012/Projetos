515f02dfc4389dda0a417b9e5b253d399367ba5f
R4fael3012 <2024b010593@a.unirn.edu.br>
2025-08-26 21:22:42 -0300

    ativ dados

--- AULA BANCO DE DADOS 2/db/commands.sql
+++ AULA BANCO DE DADOS 2/db/commands.sql
@@ -19,7 +19,7 @@ CREATE TABLE Alunos (
     Matricula INTEGER NOT NULL, 
     CPF TEXT NOT NULL,
     Serie TEXT NOT NULL,
-    ID_Composição INTEGER NOT NULL
+    ID_Composição TEXT NOT NULL
 );
 
 INSERT INTO Disciplina (ID_Disciplina, Nome, Dia, Serie) VALUES
@@ -31,6 +31,13 @@ INSERT INTO Professor (ID_Professor, Nome, ID_Disciplina, Matricula) VALUES
 (1, 'Alisson', 2, '2025b'),
 (2, 'Cristiane', 3, '2025a'),
 (3, 'Lucas', 1, '2025c');
+
+INSERT INTO Alunos (ID_Aluno, Nome, Matricula, CPF, Serie, ID_Composição) VALUES
+(1, 'Yan', 2024001, '112.351.155-30', '3º', '1'),
+(2, 'Matheus', 2024002, '100.22.552-10', '2º', '1,3'),
+(3, 'Carol', 2024003, '222.258.365-10', '3º', '1,2,3');
+
+
 
 
 
